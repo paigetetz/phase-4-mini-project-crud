afbd7e8d004fa3e12c08a8cba480f7a98a821478
paige <paigetetz@gmail.com>
2022-12-13 10:05:14 -0500

    Finished Lab

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,36 @@
+class SpicesController < ApplicationController
+
+    def index 
+        spices = Spice.all
+        render json: spices
+    end
+
+    def show
+        spice = Spice.find params[:id]
+        render json: spice
+    end
+
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+
+    def create
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    def update
+        spice = Spice.find_by(id: params[:id])
+        spice.update(spice_params)
+        render json: spice
+
+    end
+
+    def destroy
+        spice = Spice.find_by(id: params[:id])
+        spice.destroy
+        head :no_content
+    end
+
+    
+end
